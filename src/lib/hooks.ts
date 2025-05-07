b1cd903425b5769ceabfee0fbdb37b520f02caa9
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-07 16:06:50 +0100

    Write and read job item id to/from url

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -29,3 +29,18 @@ export function useJobItems(searchText: string) {
 
   return [jobItemsSliced, isLoading] as const;
 }
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActiveId(+window.location.hash.slice(1));
+    };
+    handleHashChange();
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+  return activeId;
+}
