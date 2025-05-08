ddffffc8752f3d8e3849d1b4a54365c5cde4eb2e
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-08 11:21:32 +0100

    Refactor useJobItem and add types

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,52 +2,20 @@ import { useEffect, useState } from 'react';
 import { JobItem, JobItemExpanded } from './types';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 import { useQuery } from '@tanstack/react-query';
-export function useActiveId() {
-  const [activeId, setActiveId] = useState<number | null>(null);
-  useEffect(() => {
-    const handleHashChange = () => {
-      setActiveId(+window.location.hash.slice(1));
-    };
-    handleHashChange();
-    window.addEventListener('hashchange', handleHashChange);
-    return () => {
-      window.removeEventListener('hashchange', handleHashChange);
-    };
-  }, []);
-  return activeId;
-}
-// export function useJobItem(id: number | null) {
-//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-//   const [isLoading, setIsLoading] = useState(false);
-//   useEffect(() => {
-//     if (!id) return;
 
-//     const fetchJob = async () => {
-//       setIsLoading(true);
-//       try {
-//         const response = await fetch(`${API_BASE_URL}/${id}`);
-//         const data = await response.json();
-//         setIsLoading(false);
-//         setJobItem(data?.jobItem ?? null);
-//       } catch (err) {
-//         setJobItem(null);
-//         setIsLoading(false);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-//     fetchJob();
-//   }, [id]);
-//   return { jobItem, isLoading } as const;
-// }
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: JobItemExpanded;
+};
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+  const response = await fetch(`${API_BASE_URL}/${id}`);
+  const data = await response.json();
+  return data;
+};
 export function useJobItem(id: number | null) {
   const { data, isLoading } = useQuery(
     ['job-item', id],
-    async () => {
-      const response = await fetch(`${API_BASE_URL}/${id}`);
-      const data = await response.json();
-      return data;
-    },
+    () => (id ? fetchJobItem(id) : null),
     {
       staleTime: 1000 * 60 * 60,
       refetchOnWindowFocus: false,
@@ -56,7 +24,7 @@ export function useJobItem(id: number | null) {
       onError: () => {},
     }
   );
-  const jobItem: JobItemExpanded = data?.jobItem;
+  const jobItem = data?.jobItem;
   return { jobItem, isLoading } as const;
 }
 export function useJobItems(searchText: string) {
@@ -98,3 +66,42 @@ export function useDebounce<T>(value: T, delay = 250): T {
   }, [value, delay]);
   return debouncedValue;
 }
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActiveId(+window.location.hash.slice(1));
+    };
+    handleHashChange();
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+  return activeId;
+}
+
+// export function useJobItem(id: number | null) {
+//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//   useEffect(() => {
+//     if (!id) return;
+
+//     const fetchJob = async () => {
+//       setIsLoading(true);
+//       try {
+//         const response = await fetch(`${API_BASE_URL}/${id}`);
+//         const data = await response.json();
+//         setIsLoading(false);
+//         setJobItem(data?.jobItem ?? null);
+//       } catch (err) {
+//         setJobItem(null);
+//         setIsLoading(false);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
+//     fetchJob();
+//   }, [id]);
+//   return { jobItem, isLoading } as const;
+// }
