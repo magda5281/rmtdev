0d208d52718d228151c176ee48aff557ce8b1797
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-08 13:46:52 +0100

    Add pagination controls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,12 +16,23 @@ import { useDebounce, useJobItems } from '../lib/hooks';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
+  //state
   const [searchText, setSearchText] = useState('');
   const debouncedSearchText = useDebounce(searchText);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  //derived/computed state
   const jobItemsSliced = jobItems?.slice(0, 7) || [];
   const totalNumberOfResults = jobItems?.length || 0;
+
+  //event handlers/actions
+  const handleChangePage = (direction: 'next' | 'previous') => {
+    if (direction === 'next') {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === 'previous') {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
 
   return (
     <>
@@ -40,7 +51,10 @@ function App() {
             <SortingControls />
           </SidebarTop>
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls
+            onClick={handleChangePage}
+            currentPage={currentPage}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,45 @@
-export default function PaginationControls() {
-  return <section className='pagination'></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+
+type PaginationControlsProps = {
+  currentPage: number;
+  onClick: (direction: 'next' | 'previous') => void;
+};
+export default function PaginationControls({
+  onClick,
+  currentPage,
+}: PaginationControlsProps) {
+  return (
+    <section className='pagination'>
+      <PaginationButton
+        direction='previous'
+        currentPage={currentPage}
+        onClick={() => onClick('previous')}
+      />
+      <PaginationButton
+        direction='next'
+        currentPage={currentPage}
+        onClick={() => onClick('next')}
+      />
+    </section>
+  );
+}
+
+type PaginationButtonProps = {
+  direction: 'next' | 'previous';
+  currentPage: number;
+  onClick: () => void;
+};
+function PaginationButton({
+  direction,
+  currentPage,
+  onClick,
+}: PaginationButtonProps) {
+  const previous = direction === 'previous';
+
+  return (
+    <button className='pagination__button' onClick={onClick}>
+      {previous ? <ArrowLeftIcon /> : <ArrowRightIcon />}
+      Page {previous ? currentPage - 1 : currentPage + 1}
+    </button>
+  );
 }
